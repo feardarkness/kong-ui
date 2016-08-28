30daea2093787adfbee7f0143088084eea6eebbe
Ariel Alvarado <alvarado.ariel.contact@gmail.com>
2016-08-28 19:43:31 -0400

    First try

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -1,9 +1,10 @@
-define('app',['exports'], function (exports) {
+define('app',['exports', './todo'], function (exports, _todo) {
   'use strict';
 
   Object.defineProperty(exports, "__esModule", {
     value: true
   });
+  exports.App = undefined;
 
   function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
@@ -11,11 +12,31 @@ define('app',['exports'], function (exports) {
     }
   }
 
-  var App = exports.App = function App() {
-    _classCallCheck(this, App);
+  var App = exports.App = function () {
+    function App() {
+      _classCallCheck(this, App);
 
-    this.message = 'Hello World!';
-  };
+      this.heading = 'todos';
+      this.todos = [];
+      this.todoDescription = '';
+    }
+
+    App.prototype.addTodo = function addTodo() {
+      if (this.todoDescription) {
+        this.todos.push(new _todo.Todo(this.todoDescription));
+        this.todoDescription = '';
+      }
+    };
+
+    App.prototype.removeTodo = function removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      if (index !== -1) {
+        this.todos.splice(index, 1);
+      }
+    };
+
+    return App;
+  }();
 });
 define('environment',["exports"], function (exports) {
   "use strict";
@@ -66,6 +87,26 @@ define('main',['exports', './environment'], function (exports, _environment) {
     });
   }
 });
+define('todo',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var Todo = exports.Todo = function Todo(description) {
+    _classCallCheck(this, Todo);
+
+    this.description = description;
+    this.done = false;
+  };
+});
 define('resources/index',["exports"], function (exports) {
   "use strict";
 
@@ -75,5 +116,140 @@ define('resources/index',["exports"], function (exports) {
   exports.configure = configure;
   function configure(config) {}
 });
-define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <h1>${message}</h1>\n</template>\n"; });
+define('utility',["exports"], function (exports) {
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+	exports.areEqual = areEqual;
+	function areEqual(obj1, obj2) {
+		return Object.keys(obj1).every(function (key) {
+			return obj2.hasOwnProperty(key) && obj1[key] === obj2[key];
+		});
+	};
+});
+define('web-api',['exports'], function (exports) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var latency = 200;
+  var id = 0;
+
+  function getId() {
+    return ++id;
+  }
+
+  var contacts = [{
+    id: getId(),
+    firstName: 'John',
+    lastName: 'Tolkien',
+    email: 'tolkien@inklings.com',
+    phoneNumber: '867-5309'
+  }, {
+    id: getId(),
+    firstName: 'Clive',
+    lastName: 'Lewis',
+    email: 'lewis@inklings.com',
+    phoneNumber: '867-5309'
+  }, {
+    id: getId(),
+    firstName: 'Owen',
+    lastName: 'Barfield',
+    email: 'barfield@inklings.com',
+    phoneNumber: '867-5309'
+  }, {
+    id: getId(),
+    firstName: 'Charles',
+    lastName: 'Williams',
+    email: 'williams@inklings.com',
+    phoneNumber: '867-5309'
+  }, {
+    id: getId(),
+    firstName: 'Roger',
+    lastName: 'Green',
+    email: 'green@inklings.com',
+    phoneNumber: '867-5309'
+  }];
+
+  var WebAPI = exports.WebAPI = function () {
+    function WebAPI() {
+      _classCallCheck(this, WebAPI);
+
+      this.isRequesting = false;
+    }
+
+    WebAPI.prototype.getContactList = function getContactList() {
+      var _this = this;
+
+      this.isRequesting = true;
+      return new Promise(function (resolve) {
+        setTimeout(function () {
+          var results = contacts.map(function (x) {
+            return {
+              id: x.id,
+              firstName: x.firstName,
+              lastName: x.lastName,
+              email: x.email
+            };
+          });
+          resolve(results);
+          _this.isRequesting = false;
+        }, latency);
+      });
+    };
+
+    WebAPI.prototype.getContactDetails = function getContactDetails(id) {
+      var _this2 = this;
+
+      this.isRequesting = true;
+      return new Promise(function (resolve) {
+        setTimeout(function () {
+          var found = contacts.filter(function (x) {
+            return x.id == id;
+          })[0];
+          resolve(JSON.parse(JSON.stringify(found)));
+          _this2.isRequesting = false;
+        }, latency);
+      });
+    };
+
+    WebAPI.prototype.saveContact = function saveContact(contact) {
+      var _this3 = this;
+
+      this.isRequesting = true;
+      return new Promise(function (resolve) {
+        setTimeout(function () {
+          var instance = JSON.parse(JSON.stringify(contact));
+          var found = contacts.filter(function (x) {
+            return x.id == contact.id;
+          })[0];
+
+          if (found) {
+            var index = contacts.indexOf(found);
+            contacts[index] = instance;
+          } else {
+            instance.id = getId();
+            contacts.push(instance);
+          }
+
+          _this3.isRequesting = false;
+          resolve(instance);
+        }, latency);
+      });
+    };
+
+    return WebAPI;
+  }();
+});
+define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <h1>${heading}</h1>\n\n  <form submit.trigger=\"addTodo()\">\n    <input type=\"text\" value.bind=\"todoDescription\">\n    <button type=\"submit\">Add Todo</button>\n  </form>\n  <p>Its working???</p>\n  <ul>\n    <li repeat.for=\"todo of todos\">\n      <input type=\"checkbox\" checked.bind=\"todo.done\"/>\n      <span css=\"text-decoration: ${todo.done?'line-through':'none'}\">${todo.description}</span>\n      <button click.trigger=\"removeTodo(todo)\">Remove</button>\n    </li>\n  </ul>\n</template>\n"; });
 //# sourceMappingURL=app-bundle.js.map
